e15740ed166d7f525d8b965cf83260238ce3dee1
Devon Kidd <kidddevon98@gmail.com>
2024-11-04 11:53:18 -0500

    Add unit tests for WeatherService

--- obj/Debug/net8.0/WeatherApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/WeatherApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+668e04ab79ed4fab9be8a3958476a6aa1d82e127")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf20fed4e6a6ca69134d53088ccab567ec58e392")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
